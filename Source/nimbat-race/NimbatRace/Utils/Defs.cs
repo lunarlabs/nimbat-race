c8c637dd76629baeaeed591b7377d3642063bd97
Matthias <aussieevil@hotmail.com>
2025-10-28 09:54:50 -0700

    c-sharp stuff

---
+++ Source/nimbat-race/NimbatRace/Utils/Defs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbatRace.Utils
+{
+    internal static class Defs
+    {
+    }
+}
---
+++ Source/nimbat-race/NimbatRace/Utils/PawnHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace NimbatRace.Utils
+{
+    internal static class PawnHelpers
+    {
+        internal static bool IsNimbat(this Pawn pawn) => pawn?.kindDef?.race.defName.ToLower().Contains("nimbat") ?? false;
+    }
+}
